1a453854f8b1ec6b55e4f5aa104067a276ee1f19
anna <w0nderfly@live.com>
2023-06-06 18:12:41 +0500

    Entities + dependency Postgres in pom-file

---
+++ src/main/java/com/example/diploma/entity/UserEntity.java
@@ -0,0 +1,29 @@
+package com.example.diploma.entity;
+
+import com.example.diploma.dto.Role;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String password;
+    private String username;
+    private String email;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private String phone;
+    private Role role;
+}
