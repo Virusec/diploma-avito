5243562b638d87efd131d91dbe7e3525aff8b8e8
anna <w0nderfly@live.com>
2023-06-08 17:03:52 +0500

    added constraint unique for column email (table users)

--- src/main/resources/liquibase/scripts/users.sql
+++ src/main/resources/liquibase/scripts/users.sql
@@ -2,11 +2,15 @@
 -- changeset anna:1
 create table users
 (
-        user_id     serial primary key,
-        password    varchar(100) not null,
-        email       varchar(255) not null,
-        first_name  varchar(30) not null,
-        last_name   varchar(30) not null,
-        phone       varchar(12) not null,
-        role        varchar(50) not null
+    user_id    serial primary key,
+    password   varchar(100) not null,
+    email      varchar(255) not null,
+    first_name varchar(30)  not null,
+    last_name  varchar(30)  not null,
+    phone      varchar(12)  not null,
+    role       varchar(50)  not null
 );
+
+-- changeset anna:2
+alter table users
+    add constraint email_unique unique (email);
