cb2687bb12010715104d6f06158c33f0e9645f5a
anna <w0nderfly@live.com>
2023-06-12 20:18:54 +0500

    fix

--- src/main/java/com/example/diploma/repository/AdRepository.java
+++ src/main/java/com/example/diploma/repository/AdRepository.java
@@ -1,4 +1,4 @@
-package com.example.diploma.repository;mapping
+package com.example.diploma.repository;
 import com.example.diploma.entity.AdEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
