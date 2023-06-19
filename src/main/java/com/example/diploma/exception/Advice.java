e176cdd7062facc2c6bc6005b24f44be47265a4f
anna <w0nderfly@live.com>
2023-06-19 19:04:19 +0500

    @RestControllerAdvice

--- src/main/java/com/example/diploma/exception/Advice.java
+++ src/main/java/com/example/diploma/exception/Advice.java
@@ -1,10 +1,10 @@
 package com.example.diploma.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class Advice {
     @ExceptionHandler(FindNoEntityException.class)
