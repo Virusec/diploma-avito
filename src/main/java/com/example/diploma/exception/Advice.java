ff5372e6ea54f5fcd1d4319cc1c7ac3551460e30
anna <w0nderfly@live.com>
2023-06-20 17:12:35 +0500

    login, register, set password

--- src/main/java/com/example/diploma/exception/Advice.java
+++ src/main/java/com/example/diploma/exception/Advice.java
@@ -1,14 +1,14 @@
 package com.example.diploma.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-@ControllerAdvice
+@RestControllerAdvice
 @Slf4j
 public class Advice {
     @ExceptionHandler(FindNoEntityException.class)
     public void handleException(FindNoEntityException e) {
-        log.warn("Обращение к несуществующей записи:" + e.getMessage());
+        log.warn("Обращение к несуществующей записи: " + e.getMessage());
     }
 }
--- src/main/java/com/example/diploma/service/AuthService.java
+++ src/main/java/com/example/diploma/service/AuthService.java
@@ -1,12 +1,17 @@
 package com.example.diploma.service;
 
+import com.example.diploma.dto.NewPassword;
 import com.example.diploma.dto.RegisterReq;
 import com.example.diploma.dto.Role;
 
 /**
  * @author Anatoliy Shikin
  */
+
 public interface AuthService {
     boolean login(String userName, String password);
+
     boolean register(RegisterReq registerReq, Role role);
+
+    boolean setPassword(NewPassword newPassword, String name);
 }
