acfa25e26a990a0720f2940c727c4fcbad917678
anna <w0nderfly@live.com>
2023-06-28 16:25:25 +0500

    UserDetailsManagerImpl вместо UserDetailsManager имплементирует UserDetailsService

--- src/main/java/com/example/diploma/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/diploma/service/impl/AuthServiceImpl.java
@@ -3,13 +3,12 @@ package com.example.diploma.service.impl;
 import com.example.diploma.dto.NewPassword;
 import com.example.diploma.dto.RegisterReq;
 import com.example.diploma.dto.Role;
-import com.example.diploma.dto.UserSecurity;
 import com.example.diploma.mapping.UserMapper;
 import com.example.diploma.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,13 +18,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class AuthServiceImpl implements AuthService {
-    private final UserDetailsManager manager;
+    private final UserDetailsService manager;
     private final PasswordEncoder encoder;
     private final UserMapper mapper;
 
     @Override
     public boolean login(String userName, String password) {
-        if (!manager.userExists(userName)) {
+        if (!((UserDetailsManagerImpl) manager).userExists(userName)) {
             return false;
         }
         UserDetails userDetails = manager.loadUserByUsername(userName);
@@ -34,19 +33,19 @@ public class AuthServiceImpl implements AuthService {
 
     @Override
     public boolean register(RegisterReq registerReq, Role role) {
-        if (manager.userExists(registerReq.getUsername())) {
+        if (((UserDetailsManagerImpl) manager).userExists(registerReq.getUsername())) {
             return false;
         }
         registerReq.setRole(role);
         registerReq.setPassword(encoder.encode(registerReq.getPassword()));
-        manager.createUser(new UserSecurity(mapper.registerReqDtoToEntity(registerReq)));
+        ((UserDetailsManagerImpl) manager).createUser(mapper.registerReqDtoToEntity(registerReq));
         return true;
     }
 
     @Override
     public boolean setPassword(NewPassword newPassword, String name) {
         if (encoder.matches(newPassword.getCurrentPassword(), manager.loadUserByUsername(name).getPassword())) {
-            manager.changePassword(newPassword.getCurrentPassword(), encoder.encode(newPassword.getNewPassword()));
+            ((UserDetailsManagerImpl) manager).changePassword(encoder.encode(newPassword.getNewPassword()));
             return true;
         }
         return false;
